0ed9cec3c3062d3734f92685ec767367b330c102
benclaytonunc <bclay555@live.unc.edu>
2020-10-28 23:27:37 +0000

    sf

--- src/Vec.c
+++ src/Vec.c
@@ -45,4 +45,69 @@ void* Vec_ref(const Vec *self, size_t index)
     }
 }
 
-/* TODO: Define remainder of Vec.h functions */
+void Vec_get(const Vec *self, size_t index, void *out) {
+    if ((index < self->length) && index >= 0) {
+        //self->buffer[index] = out;
+        memcpy(out, Vec_ref(self, index), self->item_size);
+    } else {
+        fprintf(stderr, "%s:%d - Out of Bounds", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }   
+}
+void Vec_set(Vec *self, size_t index, const void *value) {
+    if (index < self->length && index >= 0) {
+        memcpy(Vec_ref(self,index),value, self->item_size);
+    } else {
+        fprintf(stderr, "%s:%d - Out of Bounds", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    } 
+}
+
+bool Vec_equals(const Vec *self, const Vec *other) {
+    if(other->length == self->length) {
+        size_t a = self->length;
+        a *= self->item_size;
+        char * b = (char *) self->buffer;
+        char * c = (char *) other->buffer;
+        for (size_t i = 0; i < a; i++) {
+
+            if (b[i] != c[i]) {
+                return false;    
+            }
+        }
+        return true;
+    }
+    return false;
+
+}
+
+void Vec_splice(Vec *self, size_t index, size_t delete_count, const void *items, size_t insert_count) {
+    size_t ogLength = self->length;
+    if (index >= ogLength || index < 0) {
+        fprintf(stderr, "%s:%d - Out of Bounds", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+    size_t endLength = self->length + insert_count - delete_count;
+    char * temp = (char *) self->buffer;
+
+    if (delete_count > 0) {
+        for (size_t i = index; i < (index + delete_count); i++) {
+            char * empty = NULL;
+            Vec_set(self, i, empty);
+        }
+    }
+
+    self = realloc(self, endLength * self->item_size);
+    if(insert_count > 0) {
+        int j = 0;
+        for (size_t i = index; i < (index + insert_count); i++) {
+            Vec_set(self, i, &items[j]);
+            j++;
+        }
+    }
+    int k = index + delete_count;
+    for (size_t i = index+insert_count; i < endLength; i++) {
+        Vec_set(self, i, &temp[k]);
+        k++;
+    }
+}
--- test/unit/VecImpl.cpp
+++ test/unit/VecImpl.cpp
@@ -11,6 +11,7 @@ extern "C" {
  * and access members of a Vec struct directly.
  */
 
+
 TEST(VecImpl, value) {
     Vec v = Vec_value(2, sizeof(int16_t));
     ASSERT_EQ(0, v.length);
@@ -51,3 +52,67 @@ TEST(VecImpl, ref_out_of_bounds) {
     Vec_drop(&v);
 }
 
+TEST(VecImpl, get) {
+    Vec v = Vec_value(3, sizeof(int16_t));
+    int16_t *buffer = (int16_t*) v.buffer;
+    buffer[0] = 10;
+    buffer[1] = 20;
+    buffer[2] = 30;
+    v.length = 3;
+    int16_t a = 0;
+    Vec_get(&v,0,&a);
+    int16_t b = 0;
+    Vec_get(&v, 1, &b);
+    ASSERT_EQ(buffer[0], a);
+    ASSERT_EQ(buffer[1], b);
+}
+TEST(VecImpl, set) {
+    Vec v = Vec_value(3, sizeof(int16_t));
+    int16_t *buffer = (int16_t*) v.buffer;
+    buffer[0] = 15;
+    buffer[1] = 7;
+    buffer[2] = 2;
+    v.length = 3;
+    int16_t b = 0;
+    Vec_set(&v, 0, &b);
+    ASSERT_EQ(buffer[0], b);
+} 
+
+TEST(VecImpl, equals) {
+    Vec v = Vec_value(3, sizeof(int16_t));
+    int16_t *buffer = (int16_t*) v.buffer;
+    buffer[0] = 1;
+    buffer[1] = 2;
+    buffer[2] = 3;
+    v.length = 3;
+    
+    Vec w = Vec_value(3, sizeof(int16_t));
+    int16_t *buff2 = (int16_t*) w.buffer;
+    buff2[0] = 1;
+    buff2[1] = 2;
+    buff2[2] = 3;
+    w.length = 3;
+    ASSERT_EQ(Vec_equals(&v, &w), true);
+
+}
+TEST(VecImpl, splice1) {
+    Vec v = Vec_value(4, sizeof(int16_t));
+    int16_t *buffer = (int16_t*) v.buffer;
+    buffer[0] = 100;
+    buffer[1] = 200;
+    buffer[2] = 300;
+    buffer[3] = 400;
+    v.length = 4;
+    int16_t *items = (int16_t*)v.buffer;
+    items[0] = 800;
+    items[1] = 900;
+    Vec fin = Vec_value(5, sizeof(int16_t));
+    int16_t *result = (int16_t*)fin.buffer;
+    result[0] = 100;
+    result[1] = 200;
+    result[2] = 800;
+    result[3] = 900;
+    result[4] = 400; 
+    Vec_splice(&v, 2, 1, items, 2);
+    ASSERT_EQ(&v, &fin); 
+}
