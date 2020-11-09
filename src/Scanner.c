65ddd23c41c55d4f8d0858ffba9de341cd50aff0
benclaytonunc <bclay555@live.unc.edu>
2020-11-09 20:54:20 +0000

    sjf

--- src/Scanner.c
+++ src/Scanner.c
@@ -8,39 +8,7 @@ char nullChar = '\0';
 CharItr nextChar;
 
 static Token token_parser(CharItr *char_itr);
-static Token TokenInit(CharItr *char_itr) {
-    char peekVal;    
-    //Token nextTok = {}; 
-     
-    if (CharItr_has_next(char_itr)) {
-        peekVal = CharItr_peek(char_itr);
-        printf(" \n peekVal: %d", peekVal);
-        if (peekVal == '|') {    
-            Token nextTok = {PIPE_TOKEN, Str_from("|")};
-            return nextTok;
-        } else if(peekVal == (nullChar | EOF)){ 
-            Token nextTok = {END_TOKEN, Str_from("")};
-            return nextTok;
-        } else { 
-           char c; 
-           size_t count = 0; 
-           Str s = Str_value(sizeof(char));// Str_from(char_itr->cursor);
-           printf(" \n before while"); 
-           while(CharItr_has_next(char_itr)) {
-                c = CharItr_next(char_itr);
-                Str_set(&s, count, c);    
-                count++;
-            }
-            printf("\n past while");
-            Token nextTok = {WORD_TOKEN, s};
-            return nextTok;
-        }
-    } else { 
-            Token nextTok = {END_TOKEN, Str_from("")};
-            return nextTok;
-    }
-}
-    Scanner Scanner_value(CharItr char_itr)
+   Scanner Scanner_value(CharItr char_itr)
     {
         Token FirstTok = token_parser(&char_itr);
         Token next = {
@@ -67,31 +35,11 @@ static Token TokenInit(CharItr *char_itr) {
         return self->next; 
     }
  static Token token_parser(CharItr *char_itr) {
- /*      TokenType currentTok = self->next.type; 
-       while (currentTok == self->next.type) {
-            self->char_itr.cursor++;    
-            printf("\n before parser peekVal"); 
-            char peekVal;
-            if (CharItr_has_next(&self->char_itr)) {
-                peekVal = CharItr_peek(&self->char_itr);
-            }
-            printf("\n past parser peekVal");
-            if (peekVal == '|') {
-                self->next.type = PIPE_TOKEN;
-            } else if (peekVal == (nullChar | EOF)) {
-               self->next.type = END_TOKEN; 
-            } else {
-                self->next.type = WORD_TOKEN;
-            }         
-       }    
-       Str_drop(&self->next.lexeme);       
-       return self->next; 
-  */
     char peekVal;    
      
     if (CharItr_has_next(char_itr)) {
         peekVal = CharItr_peek(char_itr);
-        printf(" \n peekVal: %d", peekVal);
+        // printf(" \n peekVal: %d", peekVal);
         while (CharItr_has_next(char_itr) && (peekVal == ' ' | peekVal == '\t')) {
             CharItr_next(char_itr); 
             if (CharItr_has_next(char_itr)) {
@@ -99,9 +47,18 @@ static Token TokenInit(CharItr *char_itr) {
                 peekVal = CharItr_peek(char_itr); 
             }
        }
-        if (peekVal == '|') {    
-            Token nextTok = {PIPE_TOKEN, Str_from("|")};
-            return nextTok;
+        if (peekVal == '|') {     
+           
+            // Str s = Str_value(sizeof(char));
+           /* while(CharItr_has_next(char_itr)){  
+               CharItr_next(char_itr);
+           }*/ 
+           Token nextTok = {PIPE_TOKEN, Str_from("|")}; 
+           CharItr_next(char_itr); 
+           while(CharItr_has_next(char_itr) && ((CharItr_peek(char_itr) == ' ') || (CharItr_peek(char_itr) == '\t'))) {
+                CharItr_next(char_itr);
+           } 
+           return nextTok; 
         } else if(peekVal == (nullChar | EOF)){ 
             Token nextTok = {END_TOKEN, Str_from("")};
             return nextTok;
@@ -109,14 +66,18 @@ static Token TokenInit(CharItr *char_itr) {
            char c; 
            size_t count = 0; 
            Str s = Str_value(sizeof(char));// Str_from(char_itr->cursor);
-           printf(" \n before while"); 
-           while(CharItr_has_next(char_itr) && (CharItr_peek(char_itr) != ' ')){
+          // printf(" \n before while"); 
+           while(CharItr_has_next(char_itr) && (CharItr_peek(char_itr) != ' ')){ 
+                   c = CharItr_next(char_itr);
+                    Str_set(&s, count, c);    
+                    count++;
+            
                 
-                c = CharItr_next(char_itr);
-                Str_set(&s, count, c);    
-                count++;
-            }
-            printf("\n past while");
+           }
+           while(CharItr_has_next(char_itr) && ((CharItr_peek(char_itr) == ' ') || (CharItr_peek(char_itr) == '\t'))) {
+                CharItr_next(char_itr);
+           } 
+           // printf("\n past while");
             Token nextTok = {WORD_TOKEN, s};
             return nextTok;
         }
